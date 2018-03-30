9874f2dd34edd4496ff8097d340b405e03064ec9
Ryan Abis <rkabis@up.edu.ph>
2018-03-30 18:36:11 +0800

    feat(app): popupContent for feature

--- src/containers/LeafletMap/index.js
+++ src/containers/LeafletMap/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Map, TileLayer, GeoJSON } from 'react-leaflet'
+import switchFunctionTest from './switchFunctionTest'
 
 const mapboxTiles = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicmthYmlzIiwiYSI6ImNqNnZ5b2p5ZjE3OXkycW1uY2pobDJnaWgifQ.yCMd_pWrokn1fJ6xDFGvzg'
 const mapboxAttr =  'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
@@ -10,6 +11,17 @@ const mapCenter = [14.654912, 121.064264]
 const zoomLevel = 16
 
 export default class extends Component {
+	onEachFeature(feature, layer) {
+		const {
+			mapdata
+		} = this.props
+		var popupContent = switchFunctionTest(feature, mapdata)
+		layer.bindPopup(popupContent)
+		layer.on('mouseover', function (e) {
+            this.openPopup();
+        })
+	}
+
 	render() {
 		const {
 			mapdata
@@ -30,6 +42,7 @@ export default class extends Component {
 					<GeoJSON
 						key={mapdata}
 						data={mapdata ? require('./geojson/UP' + mapdata + '.json') : null}
+						onEachFeature={this.onEachFeature.bind(this)}
 					/>
 				</Map>
 			</div>
