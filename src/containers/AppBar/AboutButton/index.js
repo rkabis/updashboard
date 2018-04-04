3a8db0fa6cdfb4279b170f90b417c89e9f96256c
Ryan Abis <rkabis@up.edu.ph>
2018-04-04 12:56:04 +0800

    feat(app): partial contact us css

--- src/containers/AppBar/AboutButton/index.js
+++ src/containers/AppBar/AboutButton/index.js
@@ -50,8 +50,6 @@ const bodyStyle = {
   textAlign: 'justify'
 }
 
-// const aboutText = 'We are a group of students who love data. We believe that nothing is impossible if you have the right information, and so we want to make data as accessible as possible to everyone in the UP Diliman community.' + <br/><br/> +  'Our goal is to compile everything you’ll ever need to know about the campus, and present them on one reliable and easily-to-use platform. From looking up jeepney routes to planning your daily jogging route, our map-based interface is an easy and convenient way to master your campus. Best of all, you can access the UP Dashboard from anywhere, at any time using your laptop, tablet, or phone. Right now, we’re in the process of adding more and more features to the Dashboard. We hope you understand that the platform may contain bugs and errors, and we need your help to spot them. Simply send us your comments and suggestions using the Feedback tab below, and we’ll be sure to resolve them as quickly as possible. Finally, you can reach us directly at updashboardproject@gmail.com. Thank you, and welcome home!'
-
 export default class extends Component {
   render() {
     if(!this.props.show) {
--- src/containers/AppBar/ContactButton/index.js
+++ src/containers/AppBar/ContactButton/index.js
@@ -22,10 +22,52 @@ const modalStyle = {
   fontSize: '10pt',
 }
 
-const formButtonStyle = {
-  backgroundColor: 'black',
+const formTextStyle = {
   color: 'white',
-  width: '100px'
+  width: '170px',
+  cursor: 'pointer',
+  marginTop: '5px',
+  marginLeft: '8px'
+}
+
+const closeStyle = {
+  position: 'fixed',
+  marginTop: '-25px',
+  marginLeft: '500px',
+  fontFamily: 'Lato',
+  fontSize: '20pt',
+  color: '#3A4047',
+  cursor: 'pointer'
+}
+
+const headerStyle = {
+  position: 'fixed',
+  display: 'flex',
+  flexDirection: 'row',
+  width: '500px',
+  height: '50px',
+  backgroundColor: '#A31F25',
+  fontSize: '12pt'
+}
+
+const formStyle = {
+  marginTop: '50px'
+}
+
+const iconSize = '30px'
+
+const changeButtonStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  marginTop: '11px',
+  marginLeft: '20px'
+}
+
+const changeButtonStyleTwo = {
+  display: 'flex',
+  flexDirection: 'row',
+  marginTop: '11px',
+  marginLeft: '20px'
 }
 
 const requestForm = <iframe title='Event Request' src="https://docs.google.com/forms/d/e/1FAIpQLScR2oUzpVYisNrzAsy2voF-j1e8uUYWvPhlyEYsdvJkW9qZCQ/viewform?embedded=true" width="500" height="500" frameBorder="0" marginHeight="0" marginWidth="0">Loading...</iframe>
@@ -46,16 +88,36 @@ export default class extends Component {
     return (
       <div style={backdropStyle}>
         <div style={modalStyle}>
-          <button onClick={this.props.onClose}>
+          <div style={closeStyle} onClick={this.props.onClose}>
             x
-          </button>
-          <div style={formButtonStyle} onClick={() => this.setState({currentForm: requestForm})}>
-            request form
           </div>
-          <div style={formButtonStyle} onClick={() => this.setState({currentForm: surveyForm})}>
-            survey form
+
+          <div style={headerStyle}>
+            <div style={changeButtonStyle}>
+              <img
+                src={require('./icons/feedback.png')}
+                height={iconSize}
+                width={iconSize}
+              />
+              <div style={formTextStyle} onClick={() => this.setState({currentForm: surveyForm})}>
+                FEEDBACK
+              </div>
+            </div>
+            <div style={changeButtonStyleTwo}>
+              <img
+                src={require('./icons/request.png')}
+                height={iconSize}
+                width={iconSize}
+              />
+              <div style={formTextStyle} onClick={() => this.setState({currentForm: requestForm})}>
+                ADD YOUR EVENT
+              </div>
+            </div>
           </div>
-          {this.state.currentForm}
+
+          <div style={formStyle}>
+            {this.state.currentForm}
+          </div>
         </div>
       </div>
     )
