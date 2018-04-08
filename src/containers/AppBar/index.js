9b39bc9004b44f8476c85e6c84800f134c87c8ec
Ryan Abis <rkabis@up.edu.ph>
2018-04-08 22:52:38 +0800

    style(appbar): responsive font size to mobile

--- src/containers/AppBar/index.js
+++ src/containers/AppBar/index.js
@@ -71,7 +71,7 @@ export default class extends Component {
 				className='flex flex-row justify-between w-100 absolute top-0 left-0'
 				style={componentStyle}
 			>
-				<div className='f3' style={titleStyle}>
+				<div className='f3-l f6-ns' style={titleStyle}>
 					{'UP Dashboard'}
 				</div>
 
@@ -80,7 +80,7 @@ export default class extends Component {
 						<div
 							onClick={this.aboutModal}
 							style={buttonStyle}
-							className='f6'
+							className='f6-l f6-m f7-l'
 						>
 							ABOUT US
 						</div>
@@ -96,7 +96,7 @@ export default class extends Component {
 						<div
 							onClick={this.contactModal}
 							style={buttonStyle}
-							className='f6'
+							className='f6-l f6-m f7-l'
 						>
 							CONTACT US
 						</div>
