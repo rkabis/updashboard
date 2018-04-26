151367403b4cbb0b25f83116b9d035e8d0f5c7dc
Ryan Abis <rkabis@up.edu.ph>
2018-04-26 14:09:19 +0800

    feat(electionmodal): removed

--- src/containers/Drawer/AddFeature/index.js
+++ src/containers/Drawer/AddFeature/index.js
@@ -41,16 +41,6 @@ const titleStyle = {
   fontWeight: 'bold'
 }
 
-const backdropStyleTwo = {
-  position: 'fixed',
-  top: 0,
-  bottom: 0,
-  left: 0,
-  right: 0,
-  backgroundColor: 'rgba(0,0,0,0.3)',
-  padding: 50
-}
-
 const modalStyleTwo = {
   backgroundColor: '#fff',
   width: 300,
