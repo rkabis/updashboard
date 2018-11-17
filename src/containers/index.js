63c5a65296d822f5532e01dd408e456ee5aa7b87
Ryan Abis <ryan.abis@gmail.com>
2018-11-17 08:51:51 +0800

    basic router setup

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
+
+import Home from './Home'
+import NotFound from './NotFound'
 
 class Containers extends Component {
   render () {
-    return <div>Test</div>
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' render={() => <Home />} />
+          <Route path='*' render={() => <NotFound />} />
+        </Switch>
+      </BrowserRouter>
+	)
   }
 }
 
